45103847ee3bd0943251e1f9308d99748ee4f033
Kaptan <kaptanyadav007@gmail.com>
2020-11-01 13:53:02 +0530

    MainActivity UI ---- Animation added

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -2,16 +2,34 @@ package com.example.stopwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btnGetStarted;
+    private ImageView frontImage;
+    Animation anim_FrontImage,anim_one,anim_two;
+    private TextView title,description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnGetStarted=findViewById(R.id.btnGetStarted);
+        frontImage=findViewById(R.id.imgFront);
+        title=findViewById(R.id.textTitle);
+        description=findViewById(R.id.textDesription);
+        anim_FrontImage= AnimationUtils.loadAnimation(MainActivity.this,R.anim.anim_front_image);
+        anim_one=AnimationUtils.loadAnimation(this,R.anim.anim_one);
+        anim_two=AnimationUtils.loadAnimation(this,R.anim.anim_two);
+        frontImage.startAnimation(anim_FrontImage);
+        title.startAnimation(anim_one);
+        description.startAnimation(anim_one);
+        btnGetStarted.startAnimation(anim_two);
     }
 }
